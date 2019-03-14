d0faa599d81873a8d7728c06661873fe74702f5e
Terje Larsen <terlar@gmail.com>
2019-03-14 18:01:07 +0100

    typescript: speed test

---
+++ typescript/test-speed/tests/jasmine/math.test.ts
@@ -0,0 +1,9 @@
+import { add } from "../../src/math";
+
+import "jasmine";
+
+describe("My math library", () => {
+  it("should be able to add things correctly", () => {
+    expect(add(3, 4)).toEqual(7);
+  });
+});
---
+++ typescript/test-speed/tests/jest/math.test.ts
@@ -0,0 +1,7 @@
+import { add } from "../../src/math";
+
+describe("My math library", () => {
+    it("should be able to add things correctly", () => {
+        expect(add(3, 4)).toEqual(7);
+    });
+});
