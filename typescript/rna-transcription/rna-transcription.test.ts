a33f1e0b6cf616ce8d3e298a14cffeaad5fa507d
Terje Larsen <terlar@gmail.com>
2019-05-18 23:12:40 +0200

    typescript: solve rna-transcription

--- typescript/rna-transcription/rna-transcription.test.ts
+++ typescript/rna-transcription/rna-transcription.test.ts
@@ -1,45 +1,39 @@
-import Transcriptor from './rna-transcription'
+import Transcriptor from "./rna-transcription"
 
-describe('Transcriptor', () => {
+describe("Transcriptor", () => {
   const transcriptor = new Transcriptor()
 
-  it('transcribes cytosine to guanine', () => {
-    expect(transcriptor.toRna('C')).toEqual('G')
+  it("transcribes cytosine to guanine", () => {
+    expect(transcriptor.toRna("C")).toEqual("G")
   })
 
-  xit('transcribes guanine to cytosine', () => {
-    expect(transcriptor.toRna('G')).toEqual('C')
+  it("transcribes guanine to cytosine", () => {
+    expect(transcriptor.toRna("G")).toEqual("C")
   })
 
-  xit('transcribes adenine to uracil', () => {
-    expect(transcriptor.toRna('A')).toEqual('U')
+  it("transcribes adenine to uracil", () => {
+    expect(transcriptor.toRna("A")).toEqual("U")
   })
 
-  xit('transcribes thymine to adenine', () => {
-    expect(transcriptor.toRna('T')).toEqual('A')
+  it("transcribes thymine to adenine", () => {
+    expect(transcriptor.toRna("T")).toEqual("A")
   })
 
-  xit('transcribes all dna nucleotides to their rna complements', () => {
-    expect(transcriptor.toRna('ACGTGGTCTTAA'))
-        .toEqual('UGCACCAGAAUU')
+  it("transcribes all dna nucleotides to their rna complements", () => {
+    expect(transcriptor.toRna("ACGTGGTCTTAA")).toEqual("UGCACCAGAAUU")
   })
 
-  xit('correctly handles invalid input', () => {
-    expect(() => transcriptor.toRna('U')).toThrowError(
-      'Invalid input DNA.'
+  it("correctly handles invalid input", () => {
+    expect(() => transcriptor.toRna("U")).toThrowError("Invalid input DNA.")
+  })
+
+  it("correctly handles completely invalid input", () => {
+    expect(() => transcriptor.toRna("XXX")).toThrowError("Invalid input DNA.")
+  })
+
+  it("correctly handles partially invalid input", () => {
+    expect(() => transcriptor.toRna("ACGTXXXCTTAA")).toThrowError(
+      "Invalid input DNA."
     )
   })
-
-  xit('correctly handles completely invalid input', () => {
-    expect(() => transcriptor.toRna('XXX')).toThrowError(
-      'Invalid input DNA.'
-    )
-  })
-
-  xit('correctly handles partially invalid input', () => {
-    expect(() => transcriptor.toRna('ACGTXXXCTTAA')).toThrowError(
-      'Invalid input DNA.'
-    )
-  })
-
 })
--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,7 +1,33 @@
+type NucleotideComplement = "C" | "G" | "A" | "U"
+
+interface NucleotideComplementDictionary {
+  [key: string]: NucleotideComplement
+}
+
+const nucleotideComplements: NucleotideComplementDictionary = {
+  G: "C",
+  C: "G",
+  T: "A",
+  A: "U"
+}
+
+function lookupNucleotideComplement(nucleotide: string): NucleotideComplement {
+  const complement = nucleotideComplements[nucleotide]
+
+  if (!complement) {
+    throw new Error("Invalid input DNA.")
+  }
+
+  return complement
+}
+
 class Transcriptor {
-    toRna( /* Parameters go here */ ) {
-        // Your code here
-    }
+  toRna(dnaStrand: string): string {
+    return dnaStrand
+      .split("")
+      .map(lookupNucleotideComplement)
+      .join("")
+  }
 }
 
 export default Transcriptor
