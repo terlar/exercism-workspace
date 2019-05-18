10943090ca606497ff6ad4afbde5ecd2a331d34d
Terje Larsen <terlar@gmail.com>
2019-05-18 22:51:54 +0200

    typescript: revisit env

---
+++ typescript/.dir-locals.el
@@ -0,0 +1,3 @@
+;;; .dir-locals.el --- Local configuration -*- coding: utf-8; lexical-binding: t; -*-
+((nil . ((compile-command . "tsc"))))
+;;; -locals.el ends here
--- typescript/shell.nix
+++ typescript/shell.nix
@@ -4,9 +4,12 @@ let
   nodejs = nodejs-10_x;
 in mkShell {
   buildInputs = [
+    nodePackages.eslint_d
+    nodePackages.indium
+    nodePackages.javascript-typescript-langserver
+    nodePackages.prettier
+    nodePackages.typescript
+    nodePackages.yarn
     nodejs
-    yarn
-  ] ++ (with pkgs.nodePackages; [
-    prettier
-  ]);
+  ];
 }
