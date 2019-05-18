7e39803f1f5e0c954cb0a05a627600649508d615
Terje Larsen <terlar@gmail.com>
2019-05-18 22:16:46 +0200

    javascript: fix exercise versions

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,7 +1,5 @@
-var TwoFer = function () {};
-
-TwoFer.prototype.twoFer = function (who = 'you') {
+function twoFer(who = 'you') {
   return `One for ${who}, one for me.`;
-};
+}
 
-module.exports = TwoFer;
+module.exports = { twoFer };
