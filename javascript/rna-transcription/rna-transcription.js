e954771b45a3edf21b75e01baf88aca09555febf
Terje Larsen <terlar@gmail.com>
2019-06-01 15:36:07 +0200

    javascript: improve rna-transcription from feedback

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,24 +1,18 @@
 const nucleotideComplements = {
-  G: 'C',
-  C: 'G',
-  T: 'A',
-  A: 'U',
+  G: "C",
+  C: "G",
+  T: "A",
+  A: "U"
 };
 
-function lookupNucleotideComplement(nucleotide) {
-  const complement = nucleotideComplements[nucleotide];
-  if (!complement) {
-    throw new Error('Invalid input DNA.');
-  }
-
-  return complement;
-}
-
 function toRna(dnaStrand) {
-  return dnaStrand
-    .split('')
-    .map(lookupNucleotideComplement)
-    .join('');
+  return dnaStrand.split("").reduce((rna, nucleotide) => {
+    if (nucleotide in nucleotideComplements === false) {
+      throw new Error(`Invalid DNA strand: dnaStrand=${dnaStrand}`);
+    }
+
+    return `${rna}${nucleotideComplements[nucleotide]}`;
+  }, "");
 }
 
 module.exports = { toRna };
