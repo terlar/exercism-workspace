b3e21c2919dc5545ef470174a961922d0ebd7f67
Terje Larsen <terlar@gmail.com>
2019-06-30 18:49:25 +0200

    javascript: solve gigasecond

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,8 +1,5 @@
-//
-// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const gigasecondInMs = 10 ** 9 * 1000;
 
-export const gigasecond = () => {
-  throw new Error("Remove this statement and implement this function");
+export const gigasecond = date => {
+  return new Date(date.getTime() + gigasecondInMs);
 };
