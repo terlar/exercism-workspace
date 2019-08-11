b97116f7e51aa2a075af3d07978df35bd7fe4432
Terje Larsen <terlar@gmail.com>
2019-08-11 23:01:11 +0200

    javascript: improve setup and init space age

--- javascript/.dir-locals.el
+++ javascript/.dir-locals.el
@@ -1,5 +1,7 @@
 ;;; .dir-locals.el --- Local configuration -*- coding: utf-8; lexical-binding: t; -*-
-((nil . ((compile-command . "jest")))
+((nil . ((eval . (setq projectile-project-root
+                       (locate-dominating-file buffer-file-name ".dir-locals.el")))
+         (compile-command . "jest")))
  (js-mode . ((mode . javascript-format-on-save)
              (flymake-eslint-executable-name . "eslint_d")
              (flymake-eslint-executable-args . "--cache"))))
