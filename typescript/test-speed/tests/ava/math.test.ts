2f328012d8c365d03a3b2ff26abd71e63bf14e7b
Terje Larsen <terlar@gmail.com>
2023-01-05 15:40:13 +0100

    typescript/test-speed: remove

--- typescript/test-speed/tests/ava/math.test.ts
+++
@@ -1,8 +0,0 @@
-import { add } from "../../src/math"
-
-import test from "ava"
-
-test("My math library should be able to add things correctly", t => {
-  t.is(add(3, 4), 7)
-  t.true(5 > 2 + 2)
-})
