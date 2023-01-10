b27ab3361167586378b5a8344660fcd36b0a96cd
Terje Larsen <terlar@gmail.com>
2023-01-10 10:37:22 +0100

    rust/clock: wip

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,12 +1,29 @@
-pub struct Clock;
+#![feature(int_roundings)]
+
+use std::fmt;
+
+#[derive(Debug, PartialEq)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+    }
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        let total_minutes: i32 = (hours * 60) + minutes;
+        let actual_hours = total_minutes.div_floor(60);
+        let actual_minutes = total_minutes - (actual_hours * 60);
+
+        Self {
+            hours: actual_hours,
+            minutes: actual_minutes,
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
