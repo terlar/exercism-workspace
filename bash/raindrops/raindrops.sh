8a70501fab85298aa7f07fe9dc1fea115d36cd28
Terje Larsen <terlar@gmail.com>
2023-01-10 15:56:03 +0100

    bash/raindrops: iteration 3

--- bash/raindrops/raindrops.sh
+++ bash/raindrops/raindrops.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
+usage() {
+    echo "Usage: ${0##*/} <int>"
+    exit 1
+}
+
 main() {
     n="$1"
 
+    [[ "$#" -eq 1 ]] || usage
+    [[ "$n" =~ ^[0-9]*$ ]] || usage
+
     declare sound
+    ((n % 3 == 0)) && sound+=Pling
+    ((n % 5 == 0)) && sound+=Plang
+    ((n % 7 == 0)) && sound+=Plong
 
-    [[ $((n % 3)) -eq 0 ]] && sound+=Pling
-    [[ $((n % 5)) -eq 0 ]] && sound+=Plang
-    [[ $((n % 7)) -eq 0 ]] && sound+=Plong
-    [[ -z "$sound" ]] && sound="$n"
-
-    echo "$sound"
+    echo "${sound:-$n}"
 }
 
 main "$@"
