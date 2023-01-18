1e8c7edb27f4ba2635d0c5d5797c0d86569f04ce
Terje Larsen <terlar@gmail.com>
2023-01-18 11:13:03 +0100

    go/two-fer: iteration 2

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -1,9 +1,9 @@
-//  Twofer is short for two for one. One for you and one for me.
+// Package twofer provides a method to produce messages with intent of sharing.
 package twofer
 
 import "fmt"
 
-// ShareWith returns an intent to share something with someone else.
+// ShareWith returns a message with intent to share something with someone else.
 func ShareWith(name string) string {
 	if name == "" {
 		name = "you"
