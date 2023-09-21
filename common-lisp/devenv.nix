a9126ee61e9f3e4bce735dc42ec031b39e9ea1d5
Terje Larsen <terlar@gmail.com>
2023-09-22 01:17:38 +0200

    devenv: fix statix complaints

--- common-lisp/devenv.nix
+++ common-lisp/devenv.nix
@@ -9,26 +9,28 @@
     echo Run the tests with:
     echo 'quicklisp-run-tests *-test.lisp'
   '';
-  scripts.test-all.exec = "quicklisp-run-tests *-test.lisp";
+  scripts = {
+    test-all.exec = "quicklisp-run-tests *-test.lisp";
+
+    quicklisp-init.exec = ''
+      if [[ ! -d $HOME/quicklisp ]]; then
+        ${pkgs.expect}/bin/expect -c 'spawn quicklisp init --quicklisp-dir "$env(HOME)/quicklisp"; expect "continue." {send "\r"}'
+
+        sbcl --non-interactive --no-sysinit --no-userinit \
+          --load $HOME/quicklisp/setup.lisp \
+          --eval '(ql-util:without-prompting (ql:add-to-init-file))' \
+          --quit
+      fi
+    '';
+
+    quicklisp-run-tests.exec = ''
+      test_name="''${1%.lisp}"
+      quicklisp run -- --load "$1" --eval "($test_name:run-tests)" --quit
+    '';
+  };
 
   packages = [
     pkgs.sbcl
     pkgs.lispPackages.quicklisp
   ];
-
-  scripts.quicklisp-init.exec = ''
-    if [[ ! -d $HOME/quicklisp ]]; then
-      ${pkgs.expect}/bin/expect -c 'spawn quicklisp init --quicklisp-dir "$env(HOME)/quicklisp"; expect "continue." {send "\r"}'
-
-      sbcl --non-interactive --no-sysinit --no-userinit \
-        --load $HOME/quicklisp/setup.lisp \
-        --eval '(ql-util:without-prompting (ql:add-to-init-file))' \
-        --quit
-    fi
-  '';
-
-  scripts.quicklisp-run-tests.exec = ''
-    test_name="''${1%.lisp}"
-    quicklisp run -- --load "$1" --eval "($test_name:run-tests)" --quit
-  '';
 }
--- devenv.nix
+++ devenv.nix
@@ -6,15 +6,17 @@
     exercism configure --workspace="$PWD" --token="$token"
   '';
 
-  pre-commit.hooks.alejandra.enable = true;
-  pre-commit.hooks.black.enable = true;
-  pre-commit.hooks.deadnix.enable = true;
-  pre-commit.hooks.elm-format.enable = true;
-  pre-commit.hooks.hlint.enable = true;
-  pre-commit.hooks.luacheck.enable = true;
-  pre-commit.hooks.ormolu.enable = true;
-  pre-commit.hooks.purty.enable = true;
-  pre-commit.hooks.shellcheck.enable = true;
-  pre-commit.hooks.statix.enable = true;
-  pre-commit.hooks.stylua.enable = true;
+  pre-commit.hooks = {
+    alejandra.enable = true;
+    black.enable = true;
+    deadnix.enable = true;
+    elm-format.enable = true;
+    hlint.enable = true;
+    luacheck.enable = true;
+    ormolu.enable = true;
+    purty.enable = true;
+    shellcheck.enable = true;
+    statix.enable = true;
+    stylua.enable = true;
+  };
 }
