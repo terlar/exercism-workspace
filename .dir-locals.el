84a4e4da46ba878686327dba05d6358eda4792a5
Terje Larsen <terlar@gmail.com>
2024-07-04 12:06:02 +0200

    .dir-locals.el: fix golang project detection

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,3 @@
-((nil (project-vc-extra-root-markers . ("Cargo.toml")))
+((nil (project-vc-extra-root-markers . ("Cargo.toml" "go.mod")))
  (python-ts-mode (python-shell-interpreter . "ipython"))
  (ruby-ts-mode (minitest-use-bundler . nil)))
